3f7769b7047820addaa270b67623f86f74e14b45
Km65384 <Km65384@gmail.com>
2025-02-09 14:35:51 -0500

    KM - Updated styles and formatting.

---
+++ PROD_PdfJsonViewer_POC.UserControls/Converters/FilePathToFileNameConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PROD_PdfJsonViewer_POC.UserControls.Converters
+{
+    public class FilePathToFileNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return System.IO.Path.GetFileName((string)value);
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
